72380f81662825b472e8faf328ce4fe5fe6b5182
JoaoGuiMB <juaogui159@gmail.com>
2021-04-17 09:07:10 -0300

    feat: starting front

---
+++ codigo/frontend/src/components/Form/index.tsx
@@ -0,0 +1,15 @@
+function Form() {
+  return (
+    <div
+      style={{
+        width: "200px",
+        height: "200px",
+        backgroundColor: "red",
+      }}
+    >
+      a
+    </div>
+  );
+}
+
+export default Form;
