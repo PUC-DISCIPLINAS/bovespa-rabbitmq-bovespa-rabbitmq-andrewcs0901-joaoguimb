93e12a4c17e14e5dccc75270741f37013a595a95
andrewcs0901 <andrewcs0901@gmail.com>
2021-04-18 14:20:28 -0300

    feat: transaction item update

--- codigo/frontend/src/components/Item/index.tsx
+++ codigo/frontend/src/components/Item/index.tsx
@@ -1,14 +1,14 @@
 import { ItemProps } from './ItemProps';
 import {Styles} from './Styles'
 
-export const Item = ({ stockName, brokerName, offer }: ItemProps) =>
+export const Item = ({ stockName, brokerName, offer,sellerBrokerName,buyerBrokerName }: ItemProps) =>
 (
     <Styles>
         <div>
             <h4>Ação: {stockName}</h4>
         </div>
         <div>
-             Ofertante: {brokerName}
+            {sellerBrokerName && buyerBrokerName? <>{sellerBrokerName} &#10140; {buyerBrokerName} </>: <> Ofertante: {brokerName}</>}
         </div>
         <div>
             Oferta: <span>Qnt: {offer.quant}</span> <span>$: {offer.price}</span>
